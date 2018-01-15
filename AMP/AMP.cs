96b3fd6ce6c659b7f4c51b64ebaabac898a394c7
Unknown <andreas-reschenhofer@gmx.at>
2018-01-15 14:31:54 +0100

    Add C# project with C++ dll project

---
+++ AMP/AMP.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AMP {
+	class AMP {
+		
+		[DllImport("AMPLib", CallingConvention = CallingConvention.StdCall)]
+		extern unsafe static void square_array(float* array, int length);
+
+		static unsafe void Main() {
+			// Allocate an array
+			float[] arr = new[] { 1.0f, 2.0f, 3.0f, 4.0f };
+
+			// Square the array elements using C++ AMP
+			fixed (float* arrPt = &arr[0]) {
+				square_array(arrPt, arr.Length);
+			}
+
+			// Enumerate the results
+			foreach (var x in arr) {
+				Console.WriteLine(x);
+			}
+		}
+	}
+}
